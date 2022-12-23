2b1a2b6016a0f4aa80b73e1d4485b35add11b1fa
Maxime Carabina <maxime.carabina@epitech.eu>
2022-12-23 10:30:40 +0100

    ci(first screen): first screen completed at 98%

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,41 @@ import './App.scss';
 
 import Header from "./components/Header/Header";
 
-import albumCharlet from "./assets/charlet.png";
+import albumCharlet from "./assets/Cassette-Charlet.png";
 import raesilienceLogo from "./assets/raesilience-logo.png";
 
 function App() {
+
+  window.addEventListener('scroll', () => {
+    let topic = document.getElementById('topic');
+
+    if (topic) {
+      window.scrollY < 20 ? topic.style.visibility = 'hidden' : topic.style.visibility = 'visible';
+    }
+  });
+
   return (
     <div className="App">
-        <Header />
+      <Header />
 
-        <div className="Hero-banner">
-            {/*<img alt="" src={albumCharlet}/>*/}
-            <div className="container">
-                <div className="left">
-                    <h3>DA indépendant situé à Paris</h3>
-                    <h1>Je réalise des projets de direction artistique, design & photographie</h1>
-                </div>
-                <div className="right">
-                    <img alt="" src={raesilienceLogo}/>
-                </div>
+      <div className="Hero-banner">
+        <div className="center-album">
+          <img alt="albumCharlet" src={albumCharlet} />
+        </div>
+        <div className="container centered-axis-xy">
+            <div className="left" id="topic">
+                <h3>DA indépendant situé à Paris</h3>
+                <h1>Je réalise des projets de direction artistique, design & photographie</h1>
+            </div>
+            <div className="right">
+                <img alt="" src={raesilienceLogo}/>
             </div>
         </div>
+        <div className="container-info-scroll">
+          <h4>SCROLL</h4>
+          <h4>2022</h4>
+        </div>
+      </div>
     </div>
   );
 }
