d6a319f6c253b57b5f14c7fb36ce52190a6db97e
Maxime CARABINA <maxime.c@neosilver.fr>
2022-12-23 12:00:40 +0100

    feat(prettier & esLint): added these 2 formaters to produce a clean code

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import './App.scss';
 
-import Header from "./components/Header/Header";
+import Header from './components/Header/Header';
 
-import albumCharlet from "./assets/Cassette-Charlet.png";
-import raesilienceLogo from "./assets/raesilience-logo.png";
+import albumCharlet from './assets/Cassette-Charlet.png';
+import raesilienceLogo from './assets/raesilience-logo.png';
 
 function App() {
-
   window.addEventListener('scroll', () => {
-    let topic = document.getElementById('topic');
+    const topic = document.getElementById('topic');
 
     if (topic) {
-      window.scrollY < 20 ? topic.style.visibility = 'hidden' : topic.style.visibility = 'visible';
+      window.scrollY < 20
+        ? (topic.style.visibility = 'hidden')
+        : (topic.style.visibility = 'visible');
     }
   });
 
@@ -25,13 +26,13 @@ function App() {
           <img alt="albumCharlet" src={albumCharlet} />
         </div>
         <div className="container centered-axis-xy">
-            <div className="left" id="topic">
-                <h3>DA indépendant situé à Paris</h3>
-                <h1>Je réalise des projets de direction artistique, design & photographie</h1>
-            </div>
-            <div className="right">
-                <img alt="" src={raesilienceLogo}/>
-            </div>
+          <div className="left" id="topic">
+            <h3>DA indépendant situé à Paris</h3>
+            <h1>Je réalise des projets de direction artistique, design & photographie</h1>
+          </div>
+          <div className="right">
+            <img alt="" src={raesilienceLogo} />
+          </div>
         </div>
         <div className="container-info-scroll">
           <h4>SCROLL</h4>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,19 +2,29 @@ import React, { Component } from 'react';
 import './Header.scss';
 
 class Header extends Component {
-    render(): React.ReactNode {
-        return (
-            <div className="Header">
-                <ul>
-                    <li><a href="#">PROJECT</a></li>
-                    <li><a href="#">À PROPOS</a></li>
-                    <li><a href="#">CONTACT</a></li>
-                    <li><a href="#">BOUTIQUE</a></li>
-                </ul>
-                <div className="dot"><a></a></div>
-            </div>
-        );
-    }
+  render(): React.ReactNode {
+    return (
+      <div className="Header">
+        <ul>
+          <li>
+            <a href="#">PROJECT</a>
+          </li>
+          <li>
+            <a href="#">À PROPOS</a>
+          </li>
+          <li>
+            <a href="#">CONTACT</a>
+          </li>
+          <li>
+            <a href="#">BOUTIQUE</a>
+          </li>
+        </ul>
+        <div className="dot">
+          <a></a>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Header;
