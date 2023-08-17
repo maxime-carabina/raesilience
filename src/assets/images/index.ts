9858d51e77bf1a7927777e9a07929216bc912047
Maxime Carabina <72019005+maxime-carabina@users.noreply.github.com>
2023-08-17 15:46:54 +0200

    implement project overview (#4)

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,2 +1,4 @@
 export * from './CordesSensibles/CordesSensibles';
 export * from './CordesSensibles/CoverCordesSensibles';
+export * from './AGDT/LampeAgdt';
+export * from './StayNoir/TshirtStayNoir';
